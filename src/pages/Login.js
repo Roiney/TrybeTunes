5c90d7d8495651122dfa7920835abfcb58dd0505
Roiney Beal <nei_beal@hotmail.com>
2022-05-18 19:30:29 -0300

    validação do btn

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,13 +1,37 @@
 import React from 'react';
 
 class Login extends React.Component {
-  render() {
-    return (
-      <div>
-        <div data-testid="page-login" />
-      </div>
-    );
+  constructor() {
+    super();
+    this.state = {
+      email: '',
+    };
   }
+
+handleEmailChange = (evt) => {
+  this.setState({ email: evt.target.value });
+}
+
+handleSubmit = () => {
+  const { email, password } = this.state;
+  alert(`Welcome ${email}`);
+}
+
+render() {
+  const { email, password } = this.state;
+  const enabled = email.length > 0;
+  return (
+    <form onSubmit={ this.handleSubmit }>
+      <input
+        type="text"
+        placeholder="Email"
+        value={ this.state.email }
+        onChange={ this.handleEmailChange }
+      />
+      <button disabled={ !enabled }>Login</button>
+    </form>
+  );
+}
 }
 
 export default Login;
