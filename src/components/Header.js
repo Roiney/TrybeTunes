e3c304bea3b12107fd24567cc6a5a1276abbdc23
Roiney Beal <nei_beal@hotmail.com>
2022-05-19 16:05:43 -0300

    req 03

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,36 @@
 import React from 'react';
+import { getUser } from '../services/userAPI';
+import Loading from './Loading';
 
 class Header extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      user: '',
+      isLoading: true,
+    };
+  }
+
+  componentDidMount() {
+    getUser().then((user) => {
+      this.setState({
+        user,
+        isLoading: false,
+      });
+    });
+  }
+
   render() {
+    const { user, isLoading } = this.state;
     return (
       <header data-testid="header-component">
-        <p>TESTE</p>
+        { isLoading ? <Loading /> : (
+          <p className="user-name-paragraph">
+            Olá,
+            { '  ' }
+            <span data-testid="header-user-name">{ user.name }</span>
+          </p>
+        )}
       </header>
     );
   }
