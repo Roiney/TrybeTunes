2d14eca2134dc87b196648a20cfb6d9953f92436
Roiney Beal <nei_beal@hotmail.com>
2022-05-19 15:45:21 -0300

    implementando o HEader

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Header extends React.Component {
+  render() {
+    return (
+      <header data-testid="header-component">
+        <p>TESTE</p>
+      </header>
+    );
+  }
+}
+
+export default Header;
