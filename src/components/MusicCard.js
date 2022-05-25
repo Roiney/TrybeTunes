082bb1774c9eae02cc4bc804740dde9bd4506570
Roiney Beal <nei_beal@hotmail.com>
2022-05-25 10:47:53 -0300

    req 07

--- src/components/MusicCard.js
+++ src/components/MusicCard.js
@@ -1,17 +1,32 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 class MusicCard extends React.Component {
   render() {
-    const { music } = this.props;
+    const { trackName, previewUrl } = this.props;
     return (
       <div>
-        <label>
-          <input data-testid={ `checkbox-music-${trackId}` } type="checkbox" />
-          Favorita
-        </label>
+        <span>{ trackName }</span>
+        <audio data-testid="audio-component" src={ previewUrl } controls>
+          <track kind="captions" />
+          O seu navegador não suporta o elemento
+          {' '}
+          <code>audio</code>
+          .
+        </audio>
       </div>
     );
   }
 }
+
+MusicCard.propTypes = {
+  trackName: PropTypes.string,
+  previewUrl: PropTypes.string,
+};
+
+MusicCard.defaultProps = {
+  trackName: 'nome da faixa',
+  previewUrl: 'url da musica',
+};
 
 export default MusicCard;
--- src/pages/Album.js
+++ src/pages/Album.js
@@ -1,14 +1,84 @@
 import React from 'react';
 import Header from '../components/Header';
+import getMusics from '../services/musicsAPI';
+import MusicCard from '../components/MusicCard';
 
 class Album extends React.Component {
+  state = {
+    musics: [],
+  }
+
+  async componentDidMount() {
+    const { match: { params: { id } } } = this.props;
+    const reponseMusic = await getMusics(id);
+    this.setState({ musics: [...reponseMusic] });
+  }
+
   render() {
+    const { musics, idFavoriteMusics } = this.state;
     return (
       <div data-testid="page-album">
         <Header />
+        {
+          musics.filter((_music, index) => index === 0).map(({
+            artworkUrl100,
+            artistName,
+            collectionName,
+          }) => (
+            <div key="album-header" className="artist-container">
+              <img src={ artworkUrl100 } alt="Album art" />
+              <p
+                className="album-name"
+                data-testid="album-name"
+              >
+                { collectionName }
+              </p>
+              <p
+                className="artist-name"
+                data-testid="artist-name"
+              >
+                { artistName }
+              </p>
+            </div>
+          ))
+        }
+        <div className="tracks-container">
+          {
+            musics.filter((_music, index) => index > 0).map(({
+              trackName,
+              previewUrl,
+              trackId,
+            }) => (
+              <MusicCard
+                key={ trackId }
+                trackName={ trackName }
+                previewUrl={ previewUrl }
+                trackId={ trackId.toString() }
+                handleChange={ this.handleFavoriteChange }
+                favoriteMusics={ idFavoriteMusics }
+              />
+            ))
+          }
+        </div>
       </div>
     );
   }
 }
+
+Album.propTypes = {
+  match: PropTypes.shape({
+    params: PropTypes.shape({
+      id: PropTypes.string,
+    }),
+  }),
+};
+
+Album.defaultProps = {
+  match: PropTypes.shape({
+    params: PropTypes.shape({
+      id: PropTypes.string,
+    }),
+  }),
+};
 
 export default Album;
